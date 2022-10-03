ae32e7b597af29e9e5b57253b442e2a8a824948a
Yan180203 <114704352+Yan180203@users.noreply.github.com>
2022-10-03 15:47:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un programa para leer una serie de números
+            //hasta obtener un número superior a 100.
+
+            int num;
+            do
+            {
+                Console.Write("INGRESE UN NUMERO SEGUIDO DE ENTER: ");
+                num= int.Parse(Console.ReadLine());
+
+            }
+            while (num <= 100);
+            Console.Write("NUMERO FUERA DE RANGO: ");
+        }
+    }
+}
