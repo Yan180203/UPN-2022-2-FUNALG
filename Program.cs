4ef767c45f530dbfe95aec148681b51152f591a7
Yan180203 <114704352+Yan180203@users.noreply.github.com>
2022-10-03 15:45:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un programa para calcular la suma de los números naturales,
+            //comprendidos entre 2 números ingresados por el usuario.
+            
+            int num1, num2, resulado;
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 =int.Parse(Console.ReadLine());
+
+            resulado = num1 + num2;
+            Console.WriteLine("El resultado es: "+ resulado);
+            Console.ReadKey();
+        }
+
+    }
+}
