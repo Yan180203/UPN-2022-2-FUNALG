51b86e75abe27a4aa50d03dcfc2dbceff3660f64
Yan180203 <114704352+Yan180203@users.noreply.github.com>
2022-10-03 15:46:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un programa para imprimir la suma de los primeros números naturales imparesmenores o iguales que n.
+
+            int num,suma = 0;
+            Console.WriteLine("ESCRIBA UN NUMERO: ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i = i + 2)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("LA SUMA DE NUMEROS IMPARES ES : " + suma);
+            Console.ReadKey();
+        }
+    }
+}
