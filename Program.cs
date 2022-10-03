be44fe75540eefb3881171feed063a13853eb670
Yan180203 <114704352+Yan180203@users.noreply.github.com>
2022-10-03 15:49:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un programa que permita escribir en una pantalla la frase
+            //“¿Desea continuar? S/N”hasta que la respuesta sea 'N'
+
+            string caracter;
+
+            do
+            {
+                Console.Write("¿DESEA CONTINUAR? S/N: ");
+                caracter = Console.ReadLine();
+            }
+            while (caracter != "N");
+            Console.ReadKey();
+        }
+    }
+}
