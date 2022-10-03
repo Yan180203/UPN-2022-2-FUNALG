730798c00e52f960f28e726b69fd2ab56aae7283
Yan180203 <114704352+Yan180203@users.noreply.github.com>
2022-10-03 15:50:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un programa para leer sucesivamente números del
+            //teclado hasta que aparezca un número comprendido entre 1 y 5
+
+            int num;
+
+            do
+            {
+                Console.Write("\t\tINGRESE UN NUMERO SEGUIDO DE ENTER: ");
+                num = int.Parse(Console.ReadLine());
+            }
+            while (num <= 1 || num >= 5);
+
+        }
+    }
+}
